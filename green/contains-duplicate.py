64131432c50cb2f0a304e59cd8309044c204c33f
Yongxi Hu <huyongxi83@gmail.com>
2026-08-21 01:08:07 -0700

    Add Contains Duplicate solution

---
+++ green/contains-duplicate.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen=set()
+        for x in nums:
+            if x in seen:
+                return True
+            seen.add(x)
+        return False
