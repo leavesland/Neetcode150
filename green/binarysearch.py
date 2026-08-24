5b0fa2acfbbe85361a5b8893fcc7040627353f10
Yongxi Hu <huyongxi83@gmail.com>
2026-08-24 01:04:49 -0700

    need to try one more tmr

---
+++ green/binarysearch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        left=0
+        right=len(nums)-1
+        mid = (left+right)//2
+        while left<=right:
+            if nums[mid] == target:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
+                mid = (left+right)//2
+            else:
+                right=mid-1
+                mid = (left+right)//2
+        return -1
